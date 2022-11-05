4c603b75b96561a6372fa79d95ebc94b7acd4177
IhsanFauzan98 <114044976+IhsanFauzan98@users.noreply.github.com>
2022-11-05 16:30:17 +0800

    Add files via upload

---
+++ posttest7/home/product.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>polygonbikes.com</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+</head>
+
+<body>
+    <!--Header-->
+    <div class="medsos">
+        <div class="container">
+            <ul>
+                <li><a href="https://web.facebook.com/polygonbikesid"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="https://www.youtube.com/user/PolygonCycleID"><i class="fa-brands fa-youtube"></i></a></li>
+                <li><a href="https://www.instagram.com/polygonbikesid/?hl=id"><i class="fa-brands fa-instagram"></i></a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+    <header>
+        <div class="container">
+            <h1><img src="assets/logo_polygon-removebg-preview.png" alt="kosong"></h1>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li class="active"><a href="product.php">Product</a></li>
+                <li><a href="aboutme.php">My Self</a></li>
+                <li><a href="crude/index.php">suggestion</a></li>
+                <li><a href="">
+                        <form action="logout.php" method="post" class="tbl"><button type="submit" name="logout"
+                                class="lgt">logout</button>
+                        </form>
+                    </a></li>
+                <li><a href="#"><i class="bi-brightness-high-fill jancoklu" id="toggleDark"></i></a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!--Product-->
+    <section class="product">
+        <div class="container">
+            <h3>BIKES</h3>
+            <div class="box">
+                <div>
+                    <img src="assets/helios_a8x-removebg-preview.png" alt="product" width="100%">
+                    <p>Road Bike polygon Helios A8X <br> <b>IDR. 60.000.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Siskiu-T7-P-768x576.png" alt="product" width="100%">
+                    <p>Muntain Bike Siskiu T7<br> <b>IDR. 24.000.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Path-E5-Gent-768x576-removebg-preview.png" alt="product" width="100%">
+                    <p>E-bike path E5 GENT <br> <b>IDR. 22.500.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Bend-R2-P-768x576.png" alt="product" width="100%">
+                    <p>Adventure bike BEND R2 <br> <b>IDR. 11.000.000,-</b></p>
+                </div>
+            </div>
+            <div class="box-2">
+                <div>
+                    <img src="assets/helios_a8x-removebg-preview.png" alt="product" width="100%">
+                    <p>Road Bike polygon Helios A8X <br> <b>IDR. 60.000.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Siskiu-T7-P-768x576.png" alt="product" width="100%">
+                    <p>Muntain Bike Siskiu T7<br> <b>IDR. 24.000.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Path-E5-Gent-768x576-removebg-preview.png" alt="product" width="100%">
+                    <p>E-bike path E5 GENT <br> <b>IDR. 22.500.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Bend-R2-P-768x576.png" alt="product" width="100%">
+                    <p>Adventure bike BEND R2 <br> <b>IDR. 11.000.000,-</b></p>
+                </div>
+            </div>
+            <div class="box-3">
+                <div>
+                    <img src="assets/helios_a8x-removebg-preview.png" alt="product" width="100%">
+                    <p>Road Bike polygon Helios A8X <br> <b>IDR. 60.000.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Siskiu-T7-P-768x576.png" alt="product" width="100%">
+                    <p>Muntain Bike Siskiu T7<br> <b>IDR. 24.000.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Path-E5-Gent-768x576-removebg-preview.png" alt="product" width="100%">
+                    <p>E-bike path E5 GENT <br> <b>IDR. 22.500.000,-</b></p>
+                </div>
+                <div>
+                    <img src="assets/Bend-R2-P-768x576.png" alt="product" width="100%">
+                    <p>Adventure bike BEND R2 <br> <b>IDR. 11.000.000,-</b></p>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!--Footer-->
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; 2022 - Muhammad Ihsan Fauzan, All Right Reserved.</small>
+        </div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+
+</html>
